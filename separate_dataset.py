976205b18bb9d4e5976f51ad60fda06ad219077a
Alexis CHATELAIN <achatela@made-f0Cr5s4.clusters.42paris.fr>
2024-01-25 10:51:10 +0100

    separate_dataset done

---
+++ separate_dataset.py
@@ -0,0 +1,22 @@
+import sys
+import csv
+import pandas as pd
+
+def seperate_dataset(filename):
+    try:
+        df = pd.read_csv(filename)
+        df = df.drop(columns="ID")
+        df1 = df.iloc[:40,:]
+        df2 = df.iloc[40:,:]
+        df1.to_csv("validation_dataset.csv", index=False)
+        df2.to_csv("training_dataset.csv", index=False)
+    except Exception as e:
+        sys.exit(e)
+
+def main():
+    if len(sys.argv) != 2 or ".csv" not in sys.argv[1]:
+        sys.exit("python3 separate_dataset.py ./path_dataset.csv")
+    seperate_dataset(sys.argv[1])
+
+if __name__ == "__main__":
+    main()
